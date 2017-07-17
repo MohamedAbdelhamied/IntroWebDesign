2bbd4eac3a1eb1af6d86c048fec76bd25bbcd5b2
Mohammad Sabri <mohsmh0@gmail.com>
2017-07-17 22:33:28 +0200

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,3 +1,10 @@
-$(function() {
-  $('nav a[href^="IntroWebDesign/' + location.pathname.split("/")[1] + '"]').addClass('active');
-});
+function() {
+    var nav = document.getElementById('nav'),
+        anchor = nav.getElementsByTagName('a'),
+        current = window.location.pathname.split('/')[1];
+        for (var i = 0; i < anchor.length; i++) {
+        if(anchor[i].href == current) {
+            anchor[i].className = "active";
+        }
+    }
+};
