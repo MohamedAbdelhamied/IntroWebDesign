3be3ef10d3935e1a3a3d93b203e1f8e5d9b34bba
Mohammad Sabri <mohsmh0@gmail.com>
2017-07-19 17:20:29 +0200

    removing removeClass()

--- functions.js
+++ functions.js
@@ -6,23 +6,22 @@
         switch (arr[i])
                 { 
             case 'jsintro':
-                w3.removeClass('.nav','active');
+                
                 w3.addClass('#js','active');
                 break;
                 case 'html':
-                w3.removeClass('.nav','active');
+                
                 w3.addClass('#html','active');
                 break;
                 case 'css':
-                w3.removeClass('.nav','active');
+                
                 w3.addClass('#css','active');
                 break;
                 case 'rwd':
-                w3.removeClass('.nav','active');
+                
                 w3.addClass('#rwd','active');
                 break;
                 defualt:
-                w3.removeClass('.nav','active');
                 w3.addClass('#home','active');
         }
         }
